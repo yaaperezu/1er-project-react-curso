4ad4435b82b5ec405966d54c1d074b9882177eb7
yaaperezu <yasserperez10@gmail.com>
2018-11-27 23:43:35 -0500

    Preuba 2

--- src/App.js
+++ src/App.js
@@ -2,23 +2,87 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Navigation from './components/Navigation';
+import TodoForm from './components/TodoForm';
+import { todos } from './todos.json';
+
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      todos
+    }
+    this.handleAddTodo = this.handleAddTodo.bind(this);
+  }
+
+  removeTodo(index) {
+    this.setState({
+      todos: this.state.todos.filter((e, i) => {
+        return i !== index
+      })
+    });
+  }
+
+  handleAddTodo(todo) {
+    this.setState({
+      todos: [...this.state.todos, todo]
+    })
+  }
+
+
+
   render() {
+
+    const todos = this.state.todos.map((todo, i) => {
+      return (
+        <div className="col-md-4">
+          <div className="card mt-4">
+            <div className="card-header">
+              <h3>{todo.title}</h3>
+              <span className="badge badge-pill badge-danger ml-2">
+                {todo.priority}
+              </span>
+            </div>
+            <div className="card-body">
+              <p className="card-text">{todo.description}</p>
+              <p><mark>{todo.responsible}</mark></p>
+            </div>
+            <div className="card-footer">
+              <button
+                className="btn btn-danger"
+                onClick={this.removeTodo.bind(this, i)}>
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      )
+    });
+
+
     return (
       <div className="App">
+        <Navigation titulo="Task" ntask={this.state.todos.length} />
+
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            React
           </a>
+          <div className="container">
+            <div className="row mt-4">
+              <TodoForm onAddTodo={this.handleAddTodo}/>
+            </div>
+            <div className="row mt-4">
+              {todos}
+            </div>
+          </div>
         </header>
       </div>
     );
